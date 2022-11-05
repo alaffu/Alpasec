aa86fdad519f48cae8f2a93a39b9e0e549344428
alaffu <alafhi@gmail.com>
2022-11-04 21:32:00 -0300

    :sparkles: Parser to capture arguments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // using System;
 //
+using CommandLine;
+
 public class Program
 {
+
+    private string key = "";
+
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose message")]
+        public bool Verbose { get; set; }
+
+        [Option('a', "add-user", Required = false, HelpText = "Adds new user")]
+        public string? AddUser { get; set; }
+
+        [Option('f', "password", Required = false, HelpText = "Sets user password")]
+        public string? Password { get; set; }
+
+        [Option("files")]
+        public IEnumerable<string>? Files { get; set; }
+
+    }
+    // private IDictionary<string, string> arguments = new Dictionary<string, string>();
+    //
+    // public void ArgumentHandler()
+    // {
+    // }
+
     public static void Main(string[] args)
     {
         /* Eu separei cada modulo desse projeto dentro da pasta src/
@@ -22,10 +48,35 @@ public class Program
         // userTest.start();
 
         // Código abaixo daqui não faz mais parte dos módulos
-        foreach (var arg in args)
-        {
-            Console.WriteLine(arg);
-        }
+        // foreach (var arg in args)
+        // {
+        //     Console.WriteLine(arg);
+        // }
+        //
+        // for (int i = 0; i <= args.Length; i++)
+        // {
+        //
+        // }
+
+        Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(
+            opt =>
+            {
+                User newUser = new User();
+
+                if (opt.AddUser.Length != 0)
+                {
+                    newUser.Name = opt.AddUser;
+                }
+                if (opt.Password.Length != 0)
+                {
+                    newUser.Password = opt.Password;
+                    Console.WriteLine(newUser);
+                    Console.WriteLine(newUser.Name);
+                    Console.WriteLine(newUser.Password);
+                }
+            }
+        );
 
     }
 }
+
