8bfbb49edaa4194d59c75883256800a5270de5c1
alaffu <alafhi@gmail.com>
2022-11-04 21:34:49 -0300

    :construction: Creating class Users

---
+++ src/User.cs
@@ -0,0 +1,23 @@
+class User
+{
+    private string? Path;
+
+    public string? Name { get; set; }
+
+    public string? Password { get; set; }
+
+    public void Save()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+
+    public User()
+    {
+
+    }
+}
