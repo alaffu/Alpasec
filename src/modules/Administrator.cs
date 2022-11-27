780a8b6c9fb4f842c5a2f1e4d3be84331ef214af
alaffu <alafhi@gmail.com>
2022-11-27 16:18:22 -0300

    :sparkles: added functions for changing user

--- src/modules/Administrator.cs
+++ src/modules/Administrator.cs
@@ -25,6 +25,72 @@ class Administrator : User
         }
     }
 
+    public static void ChangeUser(string username, string newUsername)
+    {
+        // remember to check if there is a user logged
+        var loggedUser = Auth.GetLoggedUser();
+
+        void changeNameJsonFile(string name, string newName, string passwordFuture = "")
+        {
+            var userToChangeIndex = User.SearchJsonIndex(name);
+
+            string userJsonFile = File.ReadAllText(Program.usersJsonPath);
+            dynamic userJson = JsonConvert.DeserializeObject(userJsonFile);
+
+            // string name = "changed_name";
+            // string password = "changed_password";
+
+            // raise error if there the program dont find the user
+            if (userToChangeIndex != null)
+            {
+                userJson[userToChangeIndex].Name = newName;
+                string output = JsonConvert.SerializeObject(userJson);
+                File.WriteAllText(Program.usersJsonPath, output);
+            }
+        }
+
+        void renameUserDirectory(string name, string newName)
+        {
+            string pathUserDirectory = Program.usersPath + "/" + name;
+            string newPathUserDirectory = Program.usersPath + "/" + newName;
+
+            // search file and check if it exists
+            // string[] fileEntries = Directory.GetFiles(Program.usersPath + "/" + user?.Name);
+            Directory.Move(pathUserDirectory, newPathUserDirectory);
+        }
+
+        // string nameChange = "changed_name";
+        // string name = "new_user";
+
+        User? user = User.Search(username);
+
+        changeNameJsonFile(username, newUsername);
+        if (user?.Name != null)
+        {
+            renameUserDirectory(user?.Name, newUsername);
+        }
+
+
+    }
+
+    public void deleteUser(string username)
+    {
+        // delete directory user
+
+        User? user = User.Search(username);
+        string pathUserDirectory = Program.usersPath + "/" + user.Name;
+        if (Directory.Exists(pathUserDirectory))
+        {
+          Directory.Delete(pathUserDirectory);
+        }else{
+          Console.WriteLine($">> The directory: {pathUserDirectory}\n does not exist");
+        }
+
+        // string newPathUserDirectory = Program.usersPath + "/" + newName;
+
+        // delete login register
+    }
+
     public Administrator(string name, string password) : base(name, password, true)
     {
         Name = name;
